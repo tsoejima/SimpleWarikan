830007ac873dd638666590a652930129aa076f70
tsoejima <t.soejimadeveloper@gmail.com>
2020-06-11 10:41:08 +0900

    Initial Commit

---
+++ SimpleWarikan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWarikan
+//
+//  Created by 副島拓哉 on 2020/06/11.
+//  Copyright © 2020 副島拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleWarikanTests/SimpleWarikanTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleWarikanTests.swift
+//  SimpleWarikanTests
+//
+//  Created by 副島拓哉 on 2020/06/11.
+//  Copyright © 2020 副島拓哉. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleWarikan
+
+class SimpleWarikanTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
